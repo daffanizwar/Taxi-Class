f2baeff089e6f7f7b6d25dc5a1b2f1b40beee0d2
daffanizwar <daffanizwar101@gmail.com>
2021-03-16 21:48:45 +0700

    Tugas

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Taxi taxi = new Taxi();
+
+            taxi.Driver = "Jono";
+            taxi.onDuty = true;
+            taxi.NumPass = 10;
+
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi_Class
+{
+    class Taxi
+    {
+        public string Driver { get; set; }
+        public bool onDuty { get; set; }
+        public int NumPass { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", Driver);
+            Console.WriteLine("On Duty : {0}", onDuty);
+            Console.WriteLine("Number of Passenger : {0}", NumPass);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("\n{0} sedang menjemput penumpang", Driver);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", Driver);
+        }
+           
+    }
+}
